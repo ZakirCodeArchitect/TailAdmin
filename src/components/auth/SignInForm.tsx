459168cdfaa4e5697f93473cc222ed1066657419
Zakir Matloob <webdeveloper.zakir149@gmail.com>
2025-04-18 23:03:55 -0700

    google authentication implemented with logout too

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -7,6 +7,7 @@ import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "@/icons";
 import Link from "next/link";
 import React, { useState } from "react";
 import { GithubSignIn } from "./github-signin";
+import { GoogleSignIn } from "./google-signin";
 import { Github } from "@/components/ui/github";
 
 export default function SignInForm() {
@@ -35,7 +36,7 @@ export default function SignInForm() {
           </div>
           <div>
             <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-5">
-              <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
+              {/* <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
                 <svg
                   width="20"
                   height="20"
@@ -61,7 +62,8 @@ export default function SignInForm() {
                   />
                 </svg>
                 Sign in with Google
-              </button>
+              </button> */}
+              <GoogleSignIn />
               {/* <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
                 <svg
                   width="21"
