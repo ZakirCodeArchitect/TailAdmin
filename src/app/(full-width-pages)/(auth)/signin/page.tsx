966ba6a0a3ca2af74e15380814f4b804213058a5
Zakir Matloob <webdeveloper.zakir149@gmail.com>
2025-05-06 23:12:14 +0500

    stripped down the project

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -4,8 +4,8 @@ import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title: "Sign In",
+  description: "WorkAudit application",
 };
 
 export default async function SignIn() {
